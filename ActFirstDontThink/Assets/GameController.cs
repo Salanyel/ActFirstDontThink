b884b3180ad3c3f1dfbdf7eb497d6b339828cc52
Milan "Edrevan" Irigoyen <edrevan@gmail.com>
2016-04-09 18:42:42 +0200

    Started game controller

---
+++ ActFirstDontThink/Assets/GameController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GameController : MonoBehaviour {
+
+    // Use this for initialization
+    public GameObject PlayerAvatar;
+    public GameObject BotAvatar;
+    public int nbBots;
+
+    GameObject[] spawnPoints;
+
+	void Start ()
+    {
+        // Finding spawnpoints
+        spawnPoints = GameObject.FindGameObjectsWithTag("spawn");
+
+	    // CREATING EVERYOOOOOOOOOONE
+        for (int i = 0; i < nbBots; ++i)
+        {
+            GameObject newBot = GameObject.Instantiate(BotAvatar);
+            newBot.transform.position = spawnPoints[Random.Range(0, spawnPoints.Length)].transform.position;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+}
