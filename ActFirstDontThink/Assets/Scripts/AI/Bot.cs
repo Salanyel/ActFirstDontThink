cf600e07663eb5839f717ecb2045bbcda294b1c8
Milan "Edrevan" Irigoyen <edrevan@gmail.com>
2016-04-09 18:29:14 +0200

    Still moar stuff

--- ActFirstDontThink/Assets/Scripts/AI/Bot.cs
+++ ActFirstDontThink/Assets/Scripts/AI/Bot.cs
@@ -1,15 +1,48 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Bot : MonoBehaviour {
+public class Bot : MonoBehaviour
+{
+
+    enum Actions
+    {
+        TRAVELLING,
+        PLOTTING
+    };
+
+    NavMeshAgent agent;
+    Actions currentAction;
 
 	// Use this for initialization
-	void Start () {
-	
-	}
+	void Start ()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void Update ()
+    {
+	    if (agent.remainingDistance < 0.5f)
+        {
+            if (currentAction == Actions.PLOTTING)
+            {
+                // TODO: trigger closest object
+            }
+
+            actWithoutThinking();
+        }
 	}
+
+    void actWithoutThinking()
+    {
+        if (Random.Range(0,1) == 1)
+        {
+            // Travelling to a new room
+        }
+        else
+        {
+            // Going to press another button
+        }
+    }
+
 }
