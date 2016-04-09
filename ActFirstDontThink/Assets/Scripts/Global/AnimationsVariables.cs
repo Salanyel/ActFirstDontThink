9c09fb2058a2b7c0f95b1fc579f28df59678b57a
Salanyel <maxime.devecchi@gmail.com>
2016-04-09 19:51:42 +0200

    Modified :  - Animator Controller for button and inter  - Prefab for buttons  - InputManager.asset  - Script reset state after animation

---
+++ ActFirstDontThink/Assets/Scripts/Global/AnimationsVariables.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimationsVariables : MonoBehaviour {
+
+    public const string m_interactibleObject_isUsed = "m_isUsed";
+}
--- ActFirstDontThink/Assets/Scripts/Interactions/TriggerObject.cs
+++ ActFirstDontThink/Assets/Scripts/Interactions/TriggerObject.cs
@@ -10,9 +10,16 @@ public class TriggerObject : MonoBehaviour {
 
         Debug.Log(this.gameObject.name + " has been activated", this.gameObject);
 
+        playUseAnimation();
+
         foreach (ActionObject t in targets)
         {
             t.activate();
         }
+    }
+
+    void playUseAnimation()
+    {
+        GetComponent<Animator>().SetBool(AnimationsVariables.m_interactibleObject_isUsed, true);
     }
 }
