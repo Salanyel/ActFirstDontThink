5ebd40f4c0de4c5a923606caf005347be1a3e97b
Milan "Edrevan" Irigoyen <edrevan@gmail.com>
2016-04-09 16:24:25 +0200

    ET BIM

---
+++ ActFirstDontThink/Assets/Scripts/MazeGeneration/MazeBuilder.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class MazeBuilder : MonoBehaviour {
+
+    public List<GameObject> roomPrefabs;
+    public int roomSize = 10;
+    public int size = 5;
+
+    //List<GameObject> rooms;
+    int roomOffset;
+
+	// Use this for initialization
+	void Start ()
+    {
+        roomOffset = roomSize / 2;
+
+        // Creating cells
+        GameObject cell = new GameObject("Cells");
+	    for (int i = 0; i < size; ++i)
+        {
+            for (int j = 0; j < size; ++j)
+            {
+                int roomIndex = Random.Range(0, roomPrefabs.Count - 1);
+                GameObject newRoom = Instantiate(roomPrefabs[roomIndex]);
+                newRoom.transform.SetParent(cell.transform);
+                newRoom.transform.position = new Vector3(i * roomSize + roomOffset, 0, j * roomSize + roomOffset);
+                //rooms.Add(newRoom);
+            }
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ ActFirstDontThink/Assets/Scripts/TrackPosition.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TrackPosition : MonoBehaviour {
+
+    public Transform target;
+    public Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.transform.position = target.transform.position + offset;
+	}
+}
