b306aa56a7b226a9c9eb45802076f9a88438c5ec
Salanyel <maxime.devecchi@gmail.com>
2016-04-10 11:47:38 +0200

    Modified :  - action object  - interactiveObject  - camera

--- ActFirstDontThink/Assets/Scripts/Global/Tags.cs
+++ ActFirstDontThink/Assets/Scripts/Global/Tags.cs
@@ -5,4 +5,5 @@ public class Tags : MonoBehaviour {
 
     public const string m_interactibleObject = "InteractibleObject";
     public const string m_avatar = "Avatar";
+    public const string m_cameraP1 = "CameraP1";
 }
---
+++ ActFirstDontThink/Assets/Scripts/Player/CameraController.cs
@@ -0,0 +1,22 @@
+﻿
+using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+    public Vector3 m_offset;
+    public Vector3 m_offsetRotation;
+
+    void Start()
+    {
+        m_offset = new Vector3(0, 3.44f, 0);
+        m_offsetRotation = new Vector3(90, 0, 0);
+    }
+
+    public void setPosition()
+    {
+        transform.localPosition = m_offset;
+        transform.localEulerAngles = m_offsetRotation;
+        transform.localScale = new Vector3(1, 1, 1);
+    }
+}
